1d57732faa97da11caae8384b2fdbea6ce6fecc2
Hamsalekha <hamsalekharamesh@gmail.com>
2024-12-28 17:50:37 +0530

    All Files

--- src/RecordingPracticeMainMethod/Lab005.java
+++ src/RecordingPracticeMainMethod/Lab005.java
@@ -2,5 +2,13 @@ package RecordingPracticeMainMethod;
 
 public class Lab005 {
 
+    public static void main(String[] args) {
+
+        int age = 30;
+        age = age + 2;
+        System.out.println(age);
+
+    }
+
 
 }
